4be45c8ae81d515ba1a78bb672b869e4fc8c0e31
thomalu <thomalu@ssfs.org>
2019-09-09 14:15:08 -0400

    added hey jessy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello.world");
-        System.out.println("hey jessy");
+        System.out.println("This is \"Lucy's\"computer");
+        System.out.println("2:09 pm");
+        System.out.println("9/9/2019");
     }
 }
