327f266c7662377123fedec696dc039bfb989df2
thomalu <thomalu@ssfs.org>
2019-09-12 08:57:50 -0400

    Finished parts 1&2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hello.world");
-        System.out.println("This is \"Lucy's\"computer");
-        System.out.println("2:09 pm");
-        System.out.println("9/9/2019");
+        System.out.println("Hello, world");
     }
 }
--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -3,5 +3,30 @@ package com.company;
 public class Printing {
 
     public static void main(String[] args) {
+        System.out.println("02:37pm");
+        System.out.println("09/10/2019");
+        System.out.println(" ");
+        System.out.println("It is a great");
+        System.out.println("time to be a");
+        System.out.println("computer programmer!");
+        System.out.println(" ");
+        System.out.print("It is a great ");
+        System.out.print("time to be a ");
+        System.out.println("computer programmer!");
+        System.out.println(" ");
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"? Or between a \" and a \\?");
+        System.out.println(" ");
+        System.out.println("One is what we see when we're typing our program.");
+        System.out.println("The other is what appears on the \"console.\"");
+        System.out.println(" ");
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+
+   }
 }
 
