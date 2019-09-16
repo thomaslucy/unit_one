10cfa62f04a4686a9ba1c48d2abd9cee2d46ca0a
thomalu <thomalu@ssfs.org>
2019-09-16 10:48:59 -0400

    finished rectanglearea

---
+++ src/com/company/RectangleArea.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class RectangleArea
+{
+    public static void main(String[] args)
+    {
+        int lenght = 14;
+        int width = 6;
+        int Area = width*lenght;
+        System.out.print("Area = "+Area);
+    }
+}
