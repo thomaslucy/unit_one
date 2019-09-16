8461da7f45561c7cf1ac9b06c057b95a3b732cbf
thomalu <thomalu@ssfs.org>
2019-09-16 10:58:56 -0400

    finished poundstokilos

---
+++ src/com/company/PoundstoKilograms.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class PoundstoKilograms
+{
+    public static void main(String[] args)
+    {
+        double lbs = 115;
+        double kilos = lbs/2.205;
+        System.out.print(lbs+" pounds=");
+        System.out.print(kilos+"kilograms");
+    }
+}
