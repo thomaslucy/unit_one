2e65b2c3117ac07a55b3a1c874a0e45cf380b47a
thomalu <thomalu@ssfs.org>
2019-09-17 12:44:21 -0400

    made poundstokilos into scanner

--- src/com/company/PoundstoKilograms.java
+++ src/com/company/PoundstoKilograms.java
@@ -1,11 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PoundstoKilograms
 {
     public static void main(String[] args)
     {
-        double lbs = 115;
-        double kilos = lbs/2.205;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of pounds");
+        System.out.println("Enter the number of kilograms");
+        final double CONVERSION = 2.205;
+        double lbs = scan.nextInt();
+        double kilos = lbs/CONVERSION;
         System.out.print(lbs+" pounds=");
         System.out.print(kilos+"kilograms");
     }
