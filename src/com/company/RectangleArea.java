ec4235d2b6211a7849e466f9c58e60cedcf5a92a
thomalu <thomalu@ssfs.org>
2019-09-17 12:34:17 -0400

    made rectanglearea into scanner

--- src/com/company/RectangleArea.java
+++ src/com/company/RectangleArea.java
@@ -1,11 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class RectangleArea
 {
     public static void main(String[] args)
     {
-        int lenght = 14;
-        int width = 6;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the length of a rectangle");
+        System.out.println("width?");
+        int lenght = scan.nextInt();
+        int width = scan.nextInt();
         int Area = width*lenght;
         System.out.print("Area = "+Area);
     }
