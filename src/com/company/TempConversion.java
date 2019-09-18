823da1eecb38724ce429b71d0a35dfc5d45c55e3
thomalu <thomalu@ssfs.org>
2019-09-18 12:08:26 -0400

    fixed tempconversion scanner version

--- src/com/company/TempConversion.java
+++ src/com/company/TempConversion.java
@@ -1,10 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TempConversion
 {
     public static void main(String [] args)
     {
-        double fahrenheit = 79;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter degrees fahrenheit");
+        System.out.println("Enter degrees celsius");
+        double fahrenheit = scan.nextDouble();
         double celsius = (fahrenheit - 32)*5/9;
         System.out.println("Fahrenheit: "+fahrenheit+"°");
         System.out.print("Celsius: "+celsius+"°");
